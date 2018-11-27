ecdd603108bc21b9dbdf43543f2f68eacba517ec
Serena Liu <sl362@duke.edu>
2018-11-27 16:57:18 -0500

    updates to imem dmem and python

--- dmem.v
+++ dmem.v
@@ -86,7 +86,7 @@ module dmem (
 	defparam
 		altsyncram_component.clock_enable_input_a = "BYPASS",
 		altsyncram_component.clock_enable_output_a = "BYPASS",
-		altsyncram_component.init_file = "dmem.mif",
+		altsyncram_component.init_file = "notes.mif",
 		altsyncram_component.intended_device_family = "Cyclone IV E",
 		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
 		altsyncram_component.lpm_type = "altsyncram",
--- notes_to_mif.py
+++ notes_to_mif.py
@@ -1,52 +1,59 @@
-#!/usr/bin/env python
-# Years till 100
-import sys
 
-notes_arr = sys.argv[1].split(',')
+notes_arr =["Al", "Bl", "Cl","Al", "Bl", "Cl", "Al", "Bl", "Cl","Al", "Bl", "Cl" ]
 
 f= open("notes.txt","w+")
 
 f.write("DEPTH = 4096;\n" )
-f.write("WIDTH = 32;")
-f.write("ADDRESS_RADIX = HEX; ")
-f.write("DATA_RADIX = BIN;")
-f.write("CONTENT")
-f.write("BEGIN")
-
-
-
+f.write("WIDTH = 32;\n")
+f.write("ADDRESS_RADIX = HEX;\n")
+f.write("DATA_RADIX = BIN;\n")
+f.write("CONTENT\n")
+f.write("BEGIN\n")
 
 switcher = { # note: xposition
-        Al: 0, 
-        Bl: 1, 
-        Cl: 2, 
-        Dl: 3,
-        El: 4, 
-        Fl: 5, 
-        Gl: 6, 
+        "Al": 0, 
+        "Bl": 1, 
+        "Cl": 2, 
+        "Dl": 3,
+        "El": 4, 
+        "Fl": 5, 
+        "Gl": 6, 
 
-        Ah: 7, 
-        Bh: 8, 
-        Ch: 9, 
-        Dh: 10,
-        Eh: 11, 
-        Fh: 12, 
-        Gh: 13
+        "Ah": 7, 
+        "Bh": 8, 
+        "Ch": 9, 
+        "Dh": 10,
+        "Eh": 11, 
+        "Fh": 12, 
+        "Gh": 13
 
     } 
 
 note_position = []
+size = '{:032b}'.format(len(notes_arr))
 
-for i in range(0, len(notes_arr), 2):
-	note_position[i] = switcher(notes_arr[i], "")
+for i in range(0, len(notes_arr)):
+	note_position.append(switcher.get(notes_arr.pop(),0))
+        #print switcher.get(notes_arr.pop(), 0)
 
-address = 0
 
-for j in range(0, len(note_position))
-	hex_addr = hex(address)
-	bin_note = bin(note_position[j])
-	f.write(hex_addr, " : ", bin_note , ";" )
+f.write(format(0, '02x'))
+f.write(" : ")
+f.write(size)
+f.write(";\n")
+address = 1
+
+for j in range(0, len(note_position), 1):
+	hex_addr = format(address, '02x')
+	bin_note = '{:032b}'.format(note_position.pop())
+	f.write(hex_addr)
+	f.write(" : ")
+	f.write(bin_note)
+	f.write(";\n")
+	address+=1
 
 
 f.write("END;")
+
+f.close()
 
